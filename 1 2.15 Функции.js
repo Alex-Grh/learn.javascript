6ba6ab0c171a72599e66bef02776397b19dff727
Alex-Grh <alex_grh@mail.ru>
2022-04-16 21:44:38 +0300

    1

--- 1 2.15 Функции.js
+++ 1 2.15 Функции.js
@@ -1,9 +1,34 @@
-let userName = "vasia";
-function showMessage() {
-   userName = "Petia";
-   let message = "Privet" + userName;
+/*
+// Вызов функции
+function showMessage () {
+   alert ("Vsem privet");
+}
+showMessage();
+
+// Локальные переменные. Переменные внутри функции
+
+function showMessage () {
+   let message = "Priver";
    alert (message);
 }
-alert (userName);
-showMessage();
-alert (userName);
+showMessage ();
+alert (message);
+*/
+/*
+// Внешние переменные. 
+let userName1 = "Vasia";
+function showMessage () {
+   let message1 = "Privet" + userName1;
+   alert (message1);
+}
+showMessage ();
+
+// Есть доступ к внешним переменным и может их менять.
+*/
+let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+
+if (year == 2015) {
+  alert( 'Да вы знаток!' );
+} else {
+  alert( 'А вот и неправильно!' ); // любое значение, кроме 2015
+}
